738bec14b987728bd7abdc98a4676afdc815133c
bindu701 <bindu.gambhir@yahoo.co.in>
2020-01-03 22:53:14 +0530

    commit

--- hello123.py
+++ hello123.py
@@ -5,7 +5,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 options.binary_location = "/usr/bin/chromium"
-driver = webdriver.Chrome('C:/Users/bindu.gambhir/Desktop/chromedriver.exe')
+driver = webdriver.Chrome( 'C:/Users/bindu.gambhir/Desktop/chromedriver.exe')
 
 driver.get('https://www.w3.org/')
 time.sleep(3)
